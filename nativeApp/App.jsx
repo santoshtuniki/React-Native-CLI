c0526af85f188ad833152b1a836181f841450e53
Saisantoshkumar Tuniki <saisantosh.tuniki@onpasive.com>
2023-11-23 15:02:47 +0530

    multiple styles used

--- nativeApp/App.jsx
+++ nativeApp/App.jsx
@@ -1,29 +1,64 @@
 import React from 'react';
 import {
-	StyleSheet,
 	Text,
 	View,
-	Button
+	Button,
+	StyleSheet,
+	Image,
+	ImageBackground,
+	Alert,
+	TouchableHighlight,
 } from 'react-native';
 
 const styles = StyleSheet.create({
 	header: {
-		fontSize: 30
+		fontSize: 30,
+		fontWeight: 'bold',
+		marginBottom: 20,
+		backgroundColor: 'green',
+		textAlign: 'center'
 	},
 	para: {
 		fontSize: 20
 	},
+	slant: {
+		fontStyle: 'italic'
+	},
+	icon: {
+		width: 200,
+		height: 200,
+		marginTop: 20,
+		marginLeft: 5
+	},
+	container: {
+		flexDirection: 'row',
+	}
 });
 
-const App = ()=>{
+const App = () => {
+	const handlePress = (title) => {
+		Alert.alert(`${title} clicked.`)
+	}
+
 	return (
 		<View>
-			<Text style={styles.header}>Hello React Native</Text>
-			<Text style={styles.para}>Like</Text>
+			<Text style={styles.header}>
+				Hello React Native
+			</Text>
+
+			<Text style={styles.para}>
+				Like
+			</Text>
 			<Button title="Like" />
-			<Text style={styles.para}>Share</Text>
-			<Button title="Share" />
-			<Text style={styles.para}>Subscribe</Text>
+
+			<Text style={[styles.para, { fontWeight: 800, textDecorationLine: 'underline', }]}>
+				Share
+			</Text>
+			<Button title="Share" color="#841584" />
+
+			<Text style={[styles.para, styles.slant]}>
+				Subscribe
+			</Text>
 			<Button title="Subscribe" />
 		</View>
 	);
